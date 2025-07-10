e508f2296ee92e50050006dd5f7e6945dde13709
Beatriz Vioti Zuim <bzuim@cijunsp.local>
2025-07-10 16:26:03 -0300

    Realização do passo 6 do tutorial Tour Of Heroes Angular v17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero'; // Adjust path if needed
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
+
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => {
+        this.heroes = heroes;
+        if (heroes.length > 0) this.selectedHero = heroes[0]; // select first by default
+      });
+  }
+
+ onSelectById(id: string): void {
+  const selectedId = +id; // convert string to number
+  this.selectedHero = this.heroes.find(hero => hero.id === selectedId);
+}
+
 }
